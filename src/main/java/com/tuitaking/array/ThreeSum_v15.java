667ebfdeffd525cae0c8ccd052a9ac8dacba4b16
TuitaKing <735367737@qq.com>
2020-06-18 21:35:21 +0800

    add fast leetcode

--- src/main/java/com/tuitaking/array/ThreeSum_v15.java
+++ src/main/java/com/tuitaking/array/ThreeSum_v15.java
@@ -4,9 +4,29 @@ import com.tuitaking.dp.ClimbStairs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @author lijing
  */
 public class ThreeSum_v15 {
@@ -165,4 +185,96 @@ public class ThreeSum_v15 {
         }
         return ans;
     }
+    // leetcode 最快写法
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        int negSize = 0;
+        int posSize = 0;
+        int zeroSize = 0;
+        for (int v : nums) {
+            if (v < minValue) {
+                minValue = v;
+            }
+            if (v > maxValue) {
+                maxValue = v;
+            }
+            if (v > 0) {
+                posSize++;
+            } else if (v < 0) {
+                negSize++;
+            } else {
+                zeroSize++;
+            }
+        }
+        if (zeroSize >= 3) {
+            res.add(Arrays.asList(0, 0, 0));
+        }
+        if (negSize == 0 || posSize == 0) {
+            return res;
+        }
+        if (minValue * 2 + maxValue > 0) {
+            maxValue = -minValue * 2;
+        } else if (maxValue * 2 + minValue < 0) {
+            minValue = -maxValue * 2;
+        }
+
+        int[] map = new int[maxValue - minValue + 1];
+        int[] negs = new int[negSize];
+        int[] poses = new int[posSize];
+        negSize = 0;
+        posSize = 0;
+        for (int v : nums) {
+            if (v >= minValue && v <= maxValue) {
+                if (map[v - minValue]++ == 0) {
+                    if (v > 0) {
+                        poses[posSize++] = v;
+                    } else if (v < 0) {
+                        negs[negSize++] = v;
+                    }
+                }
+            }
+        }
+        Arrays.sort(poses, 0, posSize);
+        Arrays.sort(negs, 0, negSize);
+        int basej = 0;
+        for (int i = negSize - 1; i >= 0; i--) {
+            int nv = negs[i];
+            int minp = (-nv) >>> 1;
+            while (basej < posSize && poses[basej] < minp) {
+                basej++;
+            }
+            for (int j = basej; j < posSize; j++) {
+                int pv = poses[j];
+                int cv = 0 - nv - pv;
+                if (cv >= nv && cv <= pv) {
+                    if (cv == nv) {
+                        if (map[nv - minValue] > 1) {
+                            res.add(Arrays.asList(nv, nv, pv));
+                        }
+                    } else if (cv == pv) {
+                        if (map[pv - minValue] > 1) {
+                            res.add(Arrays.asList(nv, pv, pv));
+                        }
+                    } else {
+                        if (map[cv - minValue] > 0) {
+                            res.add(Arrays.asList(nv, cv, pv));
+                        }
+                    }
+                } else if (cv < nv) {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
 }
+/**
+ *
+ * 主要的思路是先指定一个位置不动，用两个指针在剩下的数组中查找，要略过相同的数值
+ *
+ * */
