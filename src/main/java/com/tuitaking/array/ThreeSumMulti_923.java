957d9ee76eb1543467e2fa68b59659b0d9bda60b
TuitaKing <735367737@qq.com>
2020-07-06 15:39:59 +0800

    add threeSumMulti 923

--- src/main/java/com/tuitaking/array/ThreeSumMulti_923.java
+++ src/main/java/com/tuitaking/array/ThreeSumMulti_923.java
@@ -40,9 +40,17 @@ package com.tuitaking.array;
 public class ThreeSumMulti_923 {
     public int threeSumMulti(int[] A, int target) {
         int res=0;
+        int[] count=new int[101];
+        for(int i = 0 ; i< A.length;i++){
+            count[A[i]]++;
+        }
         for(int i = 0 ; i < A.length ; i++){
             int slow=i+1;
             int quick=A.length-1;
+            if(count[A[i]]>1&&A[i]==A[i+1]){
+                i++;
+            }
+            int c=count[A[quick]];
             while (slow<quick){
                 int sum=A[i]+A[slow]+A[quick];
                 if(sum>target) {
@@ -50,11 +58,80 @@ public class ThreeSumMulti_923 {
                  }else if(sum<target){
                     slow++;
                 }else {
-                    res++;
-                    quick--;
+                    if(c>=1){
+                        if(A[i]!=A[slow]){
+                            res+=count[A[i]]*count[A[slow]]*count[A[quick]];
+                        }else {
+                            res+=count[A[i]]*count[A[quick]];
+                        }
+                        System.out.println("i:"+A[i]+"  "+count[A[i]]+" slow： "+A[slow]+" "+count[A[slow]]+" quick "+A[quick]+" "+count[A[quick]]);
+                        while (c>0) {
+                            slow++;
+                            quick--;
+                            c--;
+                        }
+                        c=count[A[quick]];
+                    }else {
+                        break;
+                    }
+
                 }
             }
         }
             return res;
     }
+    public int threeSumMulti_v1(int[] A, int target) {
+        int MOD = 1_000_000_007;
+
+        // Initializing as long saves us the trouble of
+        // managing count[x] * count[y] * count[z] overflowing later.
+        long[] count = new long[101];
+        int uniq = 0;
+        for (int x: A) {
+            count[x]++;
+            if (count[x] == 1)
+                uniq++;
+        }
+
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i)
+            if (count[i] > 0)
+                keys[t++] = i;
+
+        long ans = 0;
+        // Now, let's do a 3sum on "keys", for i <= j <= k.
+        // We will use count to add the correct contribution to ans.
+
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int T = target - x;
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < T) {
+                    j++;
+                } else if (y + z > T) {
+                    k--;
+                } else {  // # x+y+z == T, now calc the size of the contribution
+                    if (i < j && j < k) {
+                        ans += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        ans += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else {  // i == j == k
+                        ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
+                    ans %= MOD;
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+
 }
--- src/test/java/com/tuitaking/array/ThreeSumMulti_923Test.java
+++ src/test/java/com/tuitaking/array/ThreeSumMulti_923Test.java
@@ -6,7 +6,7 @@ public class ThreeSumMulti_923Test {
     @Test
     public void test(){
         ThreeSumMulti_923 a=new ThreeSumMulti_923();
-        int[] arr=new int[]{1,1,2,2,3,3,4,4,5,5};
-        a.threeSumMulti(arr,8);
+        int[] arr=new int[]{1,1,2,2,2,2};
+        System.out.println(a.threeSumMulti(arr,5));
     }
 }
