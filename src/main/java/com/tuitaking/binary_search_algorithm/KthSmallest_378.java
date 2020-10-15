4bf75c4f3625e7119d388b586e5310149423b0ec
TuitaKing <735367737@qq.com>
2020-10-15 10:35:39 +0800

    update leetcode 378 查找矩阵中第k大的数

--- src/main/java/com/tuitaking/binary_search_algorithm/KthSmallest_378.java
+++ src/main/java/com/tuitaking/binary_search_algorithm/KthSmallest_378.java
@@ -22,7 +22,12 @@ import java.util.PriorityQueue;
  */
 public class KthSmallest_378 {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> integers = new PriorityQueue<Integer>(matrix.length*matrix[0].length, new Comparator<Integer>() {
+        int m=matrix.length;
+        if(m==0){
+            return 0;
+        }
+        int n=matrix[0].length;
+        PriorityQueue<Integer> integers = new PriorityQueue<Integer>(m*n, new Comparator<Integer>() {
 
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -30,8 +35,8 @@ public class KthSmallest_378 {
             }
         });
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 integers.add(Integer.valueOf(matrix[i][j]));
             }
         }
@@ -42,6 +47,38 @@ public class KthSmallest_378 {
         int res=integers.poll();
         return res;
     }
+
+    public int kthSmallest_v2(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int num = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                num += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return num >= k;
+    }
+
+
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
