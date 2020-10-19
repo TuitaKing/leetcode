6af57f04884a9b019f26cfdf98b4809fc9c64f88
TuitaKing <735367737@qq.com>
2020-10-19 21:36:27 +0800

    add 两个数组之间的并集

--- src/main/java/com/tuitaking/binary_search_algorithm/Intersect_350.java
+++ src/main/java/com/tuitaking/binary_search_algorithm/Intersect_350.java
@@ -1,5 +1,10 @@
 package com.tuitaking.binary_search_algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 350. 两个数组的交集 II
  * 给定两个数组，编写一个函数来计算它们的交集。
@@ -28,8 +33,66 @@ package com.tuitaking.binary_search_algorithm;
  * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
  */
 public class Intersect_350 {
-
+    /**
+     * 该方法没有用到题目中提到顺序无关的，可以现排序然后在对比
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] intersect(int[] nums1, int[] nums2) {
+        int len1=nums1.length;
+        int len2=nums2.length;
+        if(len1<len2){
+            // 保证nums1的长度是长的那个
+            return intersect(nums2,nums1);
+        }
+        HashMap<Integer,Integer> count=new HashMap<>();
+        for(int i:nums2){
+            Integer num=count.get(i);
+            if(num==null){
+                num=1;
+            }else {
+                num++;
+            }
+            count.put(i,num);
+        }
+        List<Integer> res=new ArrayList<Integer>();
+        for(Integer i:nums1){
+            if(count.get(i)!=null&& count.get(i)>0){
+                res.add(i);
+                count.put(i,count.get(i)-1);
+            }
+        }
 
+        int[] result=new int[res.size()];
+        int j=0;
+        for(int i:res){
+            result[j]=i;
+            j++;
+        }
+        return  result;
+    }
+
+    public int[] intersect_v1(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int count = 0;
+        for( int i =0,j =0;i<nums1.length &&j<nums2.length;){
+            if(nums1[i] == nums2[j]){
+                nums1[count++] =nums1[i++];
+                j++;
+            }else if(nums1[i]<nums2[j]){ i++;}
+            else{ j++;}
+        }
+        return Arrays.copyOfRange(nums1,0,count);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1}, nums2 = {2,2};
+        Intersect_350 intersect_350=new Intersect_350();
+        int[] res=intersect_350.intersect(nums1,nums2);
+        for(int i :res){
+            System.out.println(i);
+        }
     }
 }
