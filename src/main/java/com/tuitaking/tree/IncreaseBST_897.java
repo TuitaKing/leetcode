5588df41ce0989ddc049b3f919c68c0593ba86db
TuitaKing <735367737@qq.com>
2020-11-03 15:43:32 +0800

    add 使用循环进行二叉树中序遍历

--- src/main/java/com/tuitaking/tree/IncreaseBST_897.java
+++ src/main/java/com/tuitaking/tree/IncreaseBST_897.java
@@ -8,75 +8,68 @@ import java.util.Deque;
 import java.util.List;
 
 /**
- * 将数组实现为二叉树
- * [5,3,6,2,4,null,8,1,null,null,null,7,9]
+ * 将数组实现为二叉树 [5,3,6,2,4,null,8,1,null,null,null,7,9]
  *
-      5
-    /   \
-   3     6
-  / \     \
- 2   4     8
- /        / \
- 1      7   9
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/increasing-order-search-tree
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>5 / \ 3 6 / \ \ 2 4 8 / / \ 1 7 9
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/increasing-order-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class IncreaseBST_897 {
   // 按照中序遍历二叉树
   public TreeNode increasingBST(TreeNode root) {
-    if(root==null){
-        return null;
+    if (root == null) {
+      return null;
     }
-    List<TreeNode> list=new ArrayList<>();
-    innerBST(root,list);
-    TreeNode res=new TreeNode(-1);
-    TreeNode right=res;
-    for(TreeNode node:list){
-        node.left=null;
-        node.right=null;
-        right.left=null;
-        right.right=node;
-        right=right.right;
+    List<TreeNode> list = new ArrayList<>();
+    innerBST(root, list);
+    TreeNode res = new TreeNode(-1);
+    TreeNode right = res;
+    for (TreeNode node : list) {
+      node.left = null;
+      node.right = null;
+      right.left = null;
+      right.right = node;
+      right = right.right;
     }
-      return res.right;
+    return res.right;
   }
 
-  public void innerBST(TreeNode treeNode, List<TreeNode> res){
-        if(treeNode==null){
-            return;
-        }
-        innerBST(treeNode.left, res);
-        res.add(treeNode);
-        innerBST(treeNode.right,res);
+  public void innerBST(TreeNode treeNode, List<TreeNode> res) {
+    if (treeNode == null) {
+      return;
+    }
+    innerBST(treeNode.left, res);
+    res.add(treeNode);
+    innerBST(treeNode.right, res);
   }
 
-  public TreeNode increasingBST_withLoop(TreeNode node){
-      Deque<TreeNode> stack=new ArrayDeque<>();
-      TreeNode res=new TreeNode(-1);
-      TreeNode right=res;
-      stack.push(node);
-      TreeNode top=stack.peek();
-      while (!stack.isEmpty()){
-          if(top.left!=null ){
-              stack.push(top.left);
-              top=top.left;
-              continue;
-          }
-          top=stack.pop();
-          right.right=new TreeNode(top.val);
-          right=right.right;
-          if(top.right!=null){
-              stack.push(top.right);
-          }
+  public TreeNode increasingBST_withLoop(TreeNode root) {
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode tmp = new TreeNode(-1);
+    TreeNode res = tmp;
+    // List<TreeNode> treeNodes=new ArrayList<>();
+
+    TreeNode node = root;
+    while (node != null || !stack.isEmpty()) {
+      while (node != null) {
+        stack.push(node);
+        node = node.left;
       }
-      return res.right;
+      node = stack.poll();
+      res.right = new TreeNode(node.val);
+      res = res.right;
+      //          treeNodes.add(node);
+      node = node.right;
+    }
+    return tmp.right;
   }
 
   public static void main(String[] args) {
-   TreeNode treeNode= TreeUtils.generateArrayToTree(new Integer[]{5,3,6,2,4,null,8,1,null,null,null,7,9});
-   IncreaseBST_897 increaseBST_897=new IncreaseBST_897();
-   increaseBST_897.increasingBST_withLoop(treeNode);
+    TreeNode treeNode =
+        TreeUtils.generateArrayToTree(
+            new Integer[] {5, 3, 6, 2, 4, null, 8, 1, null, null, null, 7, 9});
+    IncreaseBST_897 increaseBST_897 = new IncreaseBST_897();
+    increaseBST_897.increasingBST_withLoop(treeNode);
   }
 }
