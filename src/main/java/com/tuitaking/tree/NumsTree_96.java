17ba1804a5402b7f97331db4407e2ea316c8d7c3
TuitaKing <735367737@qq.com>
2020-11-25 19:11:14 +0800

    add 树转换成链表

--- src/main/java/com/tuitaking/tree/NumsTree_96.java
+++ src/main/java/com/tuitaking/tree/NumsTree_96.java
@@ -1,5 +1,8 @@
 package com.tuitaking.tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
  *
@@ -22,5 +25,41 @@ package com.tuitaking.tree;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class NumsTree_96 {
+    Deque<TreeNode> treeNodes=new ArrayDeque<>();
 
+    public void flatten(TreeNode root) {
+
+        TreeNode right=root;
+        root.left=null;
+        pre(root);
+        treeNodes.poll();
+        for(TreeNode node:treeNodes){
+            node.left=null;
+            right.right=node;
+            right=right.right;
+        }
+
+    }
+    public void pre(TreeNode node) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.add(node);
+        while (!stack.isEmpty()) {
+            TreeNode top = stack.poll();
+            System.out.println(top.val);
+            treeNodes.add(top); //处理的函数
+            if (top.right != null) {
+                stack.push(top.right);
+            }
+            if (top.left != null) {
+                stack.push(top.left);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode=TreeUtils.generateArrayToTree(new Integer[]{1,2,5,3,4,null,6});
+        NumsTree_96 numsTree_96=new NumsTree_96();
+        numsTree_96.flatten(treeNode);
+        System.out.println("hello");
+    }
 }
