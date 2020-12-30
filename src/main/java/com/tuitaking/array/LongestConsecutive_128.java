c59dca31e683e9c03899a1a6da19217fe623c643
TuitaKing <735367737@qq.com>
2020-12-30 14:58:21 +0800

    add 桶排序

--- src/main/java/com/tuitaking/array/LongestConsecutive_128.java
+++ src/main/java/com/tuitaking/array/LongestConsecutive_128.java
@@ -1,7 +1,9 @@
 package com.tuitaking.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -77,6 +79,70 @@ public class LongestConsecutive_128 {
         }
         return res;
     }
+    private int indexFor(int a, int min, int step) {
+        return (a - min) / step;
+    }
 
+    /**
+     * 桶排序
+     * @param arr
+     * @return
+     */
+    public int bucketSort(int[] arr) {
+
+        int max = arr[0], min = arr[0];
+        for (int a : arr) {
+            if (max < a)
+                max = a;
+            if (min > a)
+                min = a;
+        }
+        // 該值也可根據實際情況選擇
+        int bucketNum = max / 10 - min / 10 + 1;
+        List buckList = new ArrayList<List<Integer>>();
+        // create bucket
+        for (int i = 1; i <= bucketNum; i++) {
+            buckList.add(new ArrayList<Integer>());
+        }
+        // push into the bucket
+        for (int i = 0; i < arr.length; i++) {
+            int index = indexFor(arr[i], min, 10);
+            ((ArrayList<Integer>) buckList.get(index)).add(arr[i]);
+        }
+        ArrayList<Integer> bucket = null;
+        int index = 0;
+        for (int i = 0; i < bucketNum; i++) {
+            bucket = (ArrayList<Integer>) buckList.get(i);
+            insertSort(bucket);
+            for (int k : bucket) {
+                arr[index++] = k;
+            }
+        }
+        int res=0;
+        int count=1;
+        int tmp=arr[0];
+        for(int i= 1; i < arr.length;i++){
+            if(tmp-arr[i]==-1){
+                count++;
+            }else {
+                count=1;
+            }
+            tmp=arr[i];
+            res=Math.max(count,res);
+        }
+        return res;
+    }
+
+    // 把桶內元素插入排序
+    private void insertSort(List<Integer> bucket) {
+        for (int i = 1; i < bucket.size(); i++) {
+            int temp = bucket.get(i);
+            int j = i - 1;
+            for (; j >= 0 && bucket.get(j) > temp; j--) {
+                bucket.set(j + 1, bucket.get(j));
+            }
+            bucket.set(j + 1, temp);
+        }
+    }
 
 }
--- src/test/java/com/tuitaking/array/LongestConsecutive_128T.java
+++ src/test/java/com/tuitaking/array/LongestConsecutive_128T.java
@@ -7,9 +7,10 @@ public class LongestConsecutive_128T {
     @Test
     public void test(){
         LongestConsecutive_128  longestConsecutive_128=new LongestConsecutive_128();
-//        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{100,4,200,1,3,2}),4);
-//        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{0,3,7,2,5,8,4,6,0,1}),9);
-//        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{1,2,0,1}),3);
+        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{100,4,200,1,3,2}),4);
+        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{0,3,7,2,5,8,4,6,0,1}),9);
+        Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{1,2,0,1}),3);
         Assert.assertEquals(longestConsecutive_128.longestConsecutive_v1(new int[]{1,0,-1}),3);
+        Assert.assertEquals(longestConsecutive_128.bucketSort(new int[]{1,0,-1}),3);
     }
 }
